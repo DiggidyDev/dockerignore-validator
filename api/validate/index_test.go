222ec465f8b4d92a15030b734dbd5f9d9976a940
DiggidyDev <valentinewilson@gmail.com>
2024-03-13 18:22:38 +0000

    Added extra test to verify string lengths are what's expected

--- api/validate/index_test.go
+++ api/validate/index_test.go
@@ -26,6 +26,14 @@ func TestExtractKeysFromRequest_ShouldNotError(t *testing.T) {
     if err != nil {
         t.Errorf("Unexpected error: %s", err)
     }
+
+    if len(dockerignoreStrings) != 1 {
+        t.Errorf("Expected 1 dockerignore string, got %d", len(dockerignoreStrings))
+    }
+
+    if len(fileStrings) != 2 {
+        t.Errorf("Expected 2 file strings, got %d", len(fileStrings))
+    }
 }
 
 func TestExtractKeysFromRequest_ShouldThrowMissingDockerfileKey(t *testing.T) {
