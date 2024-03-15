d83600209ef8cb1e8196f35dd14595a5e771b32a
DiggidyDev <valentinewilson@gmail.com>
2024-03-15 11:31:56 +0000

    Initial UI for interacting with the validate API endpoint

---
+++ src/app/components/Label/Label.tsx
@@ -0,0 +1,17 @@
+interface LabelProps {
+    htmlFor: string;
+    text: string;
+
+    className?: string;
+}
+
+export function Label({ className, text, htmlFor }: LabelProps) {
+    return (
+        <label
+            htmlFor={htmlFor}
+            className={`text-2xl font-bold mb-4 ${className}`}
+        >
+            {text}
+        </label>
+    );
+}
---
+++ src/app/components/TextArea/TextArea.tsx
@@ -0,0 +1,67 @@
+import type { ChangeEvent, HTMLProps } from "react";
+import { Dispatch, SetStateAction } from "react";
+import { Label } from "../Label/Label";
+
+interface TextAreaProps {
+    label: string;
+
+    disabled?: boolean;
+    name?: string;
+    required?: boolean;
+    setValue?: Dispatch<SetStateAction<string>>;
+    value?: string;
+}
+
+export function TextArea({
+    label,
+    disabled = false,
+    name,
+    required = false,
+    setValue,
+    value,
+}: TextAreaProps) {
+    name = name ?? label.toLowerCase();
+
+    return (
+        <div className="flex w-full flex-col text-center">
+            <Label text={label} htmlFor={name} />
+            <StyledTextArea
+                disabled={disabled}
+                name={name}
+                onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                    setValue?.(e.target.value)
+                }
+                required={required}
+                spellCheck={false}
+                value={value}
+            />
+        </div>
+    );
+}
+
+interface LoadingProps {
+    label?: string;
+}
+
+TextArea.Loading = ({ label = "Loading..." }: LoadingProps) => (
+    <div className="flex w-full flex-col text-center">
+        <Label text={label} />
+        <StyledTextArea
+            additionalClasses="duration-1000 animate-pulse disabled:bg-gray-300"
+            disabled
+        />
+    </div>
+);
+
+interface StyledTextAreaProps extends HTMLProps<HTMLTextAreaElement> {
+    additionalClasses?: string;
+}
+
+function StyledTextArea({ additionalClasses, ...props }: StyledTextAreaProps) {
+    return (
+        <textarea
+            {...props}
+            className={`w-full h-full p-2 disabled:bg-gray-300 dark:disabled:bg-slate-800 disabled:cursor-not-allowed dark:bg-slate-700 rounded-md drop-shadow-lg dark:shadow-2xl resize-none ${additionalClasses}`}
+        />
+    );
+}
