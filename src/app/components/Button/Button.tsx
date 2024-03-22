272f1b40567d30d66632296f30e1fc4ff2007de6
DiggidyDev <valentinewilson@gmail.com>
2024-03-22 03:31:34 +0000

    - Fixed main content layout for larger screens (space evenly) - Button and TextArea dark/light theme support for states

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export function Button({ children, className, ...props }: ButtonProps) {
     return (
         <button
-            className={`bg-blue-500 text-white p-4 rounded-md disabled:cursor-not-allowed enabled:hover:bg-blue-600 transition-colors ${className}`}
+            className={`bg-blue-500 text-white p-4 rounded-md dark:disabled:bg-blue-800 dark:disabled:text-gray-400 disabled:bg-blue-400 disabled:cursor-not-allowed enabled:hover:bg-blue-600 transition-colors ${className}`}
             {...props}
         >
             {children}
